dbc9001578eb4cc88b22151f069e4b7f1fee0bac
joon <joon0829@naver.com>
2021-03-17 13:47:55 +0900

    First Test

---
+++ naverWebtoon.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from pprint import pprint
+import requests
+
+html = requests.get("https://comic.naver.com/webtoon/weekday.nhn")
+soup = BeautifulSoup(html.text,"html.parser")
+
+#pprint(soup)
+
+data1=soup.find('div',{'class':'col_selected'})
+# pprint(data1)
+
+
+data2=data1.findAll('a',{'class':'title'})
+# pprint(data2)
+
+titleList =[]
+for i in data2 :
+    titleList.append(i.text)
+
+pprint(titleList)
